2b2aa03eaca8beb583b2aca2efc0cb3a1275d242
Apurbo73 <apurbodebnath895@gmail.com>
2025-04-01 14:17:35 +0600

    done

---
+++ a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    // find sum of natural numbers from 1 to n
+    int n;
+    cin >> n;
+    int b = 0;
+    for (int i = 1; i <= n; i++)
+    {
+
+        b = b + i;
+    }
+    cout << b << endl;
+}
